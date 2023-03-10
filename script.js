749e950213158fc4424f885209a1308694749220
devolo13 <devolo13@gmail.com>
2023-03-09 19:25:56 -0600

    First crack at CSSing

---
+++ script.js
@@ -0,0 +1,16 @@
+var visCounter = document.getElementById('visCount');
+visCounter = Math.floor(Math.random() * 100) + 1;
+visCount.innerText = visCounter;
+
+if (visCounter % 10 == 1 && visCounter % 100 !== 11) {
+  visCount.innerText += 'ST';
+} else if (visCounter % 10 == 2 && visCounter % 100 !== 12) {
+  visCount.innerText += 'ND';
+} else if (visCounter % 10 == 3 && visCounter % 100 !== 13) {
+  visCount.innerText += 'RD';
+} else {
+  visCount.innerText += 'TH';
+}
+if (visCounter === 69) {
+  nice.innerText += ' nice';
+}
