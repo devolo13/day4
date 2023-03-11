ca04a4ba9e59ab0dd6b1bbcd9f62694fe257fef6
devolo13 <devolo13@gmail.com>
2023-03-11 11:09:04 -0600

    Updated github link and increased RNG

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var visCounter = document.getElementById('visCount');
 visCounter = Math.floor(Math.random() * 100) + 1;
 visCount.innerText = visCounter;
 
-if (visCounter % 10 == 1 && visCounter % 100 !== 11) {
+if (visCounter % 10 == 1 && visCounter % 10000 !== 11) {
   visCount.innerText += 'ST';
 } else if (visCounter % 10 == 2 && visCounter % 100 !== 12) {
   visCount.innerText += 'ND';
